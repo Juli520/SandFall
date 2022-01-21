71172e4a01b7155c588fe2835c278f4c70ade000
Julián Mei <50522986+Juli520@users.noreply.github.com>
2022-01-21 00:25:08 -0300

    Main

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float distance;
+
+    private float _maxY = 70;
+    private float _minY = -10;
+    private float _currentX;
+    private float _currentY;
+    
+    private Transform _target;
+
+    private void Awake()
+    {
+        _target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _currentX += Input.GetAxis("Mouse X");
+        _currentY -= Input.GetAxis("Mouse Y");
+
+        _currentY = Mathf.Clamp(_currentY, _minY, _maxY);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(_currentY,_currentX,0);
+        transform.position = _target.position + rotation * dir;
+        transform.LookAt(_target.position);
+    }
+}
---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _rb.useGravity = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+            StartCoroutine(WaitForFall());
+    }
+
+    IEnumerator WaitForFall()
+    {
+        yield return new WaitForSeconds(1.5f);
+        
+        _rb.useGravity = true;
+        _rb.constraints = RigidbodyConstraints.None;
+
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+}
