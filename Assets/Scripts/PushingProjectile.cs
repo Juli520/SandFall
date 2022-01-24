6ed35837cb8832fcfc9e8379b19bec6da900f3fb
Julián Mei <50522986+Juli520@users.noreply.github.com>
2022-01-23 23:25:53 -0300

    Created push shoot

---
+++ Assets/Scripts/PushingProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PushingProjectile : MonoBehaviour
+{
+    public float force;
+    public float speed;
+
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            collision.gameObject.GetComponent<Rigidbody>();
+            collision.rigidbody.AddForce(Vector3.forward * force, ForceMode.Impulse);
+            Destroy(this.gameObject);
+        }
+    }
+}
