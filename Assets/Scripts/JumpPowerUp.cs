7f9ddd17bf1526b99b30fdda6440ffe7869c6846
Federico Botta <fedebotta@gmail.com>
2022-01-24 22:53:58 -0300

    Power Up Spawner

--- Assets/Scripts/JumpPowerUp.cs
+++ Assets/Scripts/JumpPowerUp.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class JumpPowerUp : MonoBehaviour
 {
     public float duration = 5f;
     public float multiplier = 2f;
-    
+    public float timeToDestroy = 10f;
+
+    private void Awake()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 8)
