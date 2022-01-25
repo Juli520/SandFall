5bab61283b4d0ff3d5a456c982f4e54408b0686a
Federico Botta <fedebotta@gmail.com>
2022-01-24 21:47:20 -0300

    Added Jump Power Up

---
+++ Assets/Scripts/JumpPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPowerUp : MonoBehaviour
+{
+    public float duration = 5f;
+    public float multiplier = 2f;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            other.gameObject.GetComponent<PlayerState>().ApplyBuff(multiplier, duration);
+            Destroy(gameObject);
+        }
+    }
+}
