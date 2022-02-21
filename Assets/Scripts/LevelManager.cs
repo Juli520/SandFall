ef6edae6a5e63e1f62520bb89b1e19b608f5ac9d
Federico Botta <fedebotta@gmail.com>
2022-02-21 19:37:01 -0300

    Animation Sync

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviourPun
 {
-    public GameObject player;
+    public GameObject[] players;
     public Transform[] spawnPoints;
 
     [Header("Scene Changer")] 
@@ -18,13 +18,13 @@ public class LevelManager : MonoBehaviourPun
     private void Start()
     {
         if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
-            PhotonNetwork.Instantiate("Prefabs/" + player.name, spawnPoints[0].position, player.transform.rotation);
+            PhotonNetwork.Instantiate("Prefabs/" + players[0].name, spawnPoints[0].position, Quaternion.identity);
         else if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
-            PhotonNetwork.Instantiate("Prefabs/" + player.name, spawnPoints[1].position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Prefabs/" + players[1].name, spawnPoints[1].position, Quaternion.identity);
         else if (PhotonNetwork.LocalPlayer.ActorNumber == 3)
-            PhotonNetwork.Instantiate("Prefabs/" + player.name, spawnPoints[2].position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Prefabs/" + players[1].name, spawnPoints[2].position, Quaternion.identity);
         else if (PhotonNetwork.LocalPlayer.ActorNumber == 4)
-            PhotonNetwork.Instantiate("Prefabs/" + player.name, spawnPoints[3].position, Quaternion.identity);
+            PhotonNetwork.Instantiate("Prefabs/" + players[1].name, spawnPoints[3].position, Quaternion.identity);
     }
 
     //private void Update()
